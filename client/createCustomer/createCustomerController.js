1265a53cf45dc94315c44790395d9336ec054924
nicolasiscoding <nicolascoding@gmail.com>
2017-04-12 22:13:46 -0400

    WIP commit. Emails need to be removed

--- client/createCustomer/createCustomerController.js
+++ client/createCustomer/createCustomerController.js
@@ -4,7 +4,14 @@
   angular.module('createCustomerController', [])
   .controller('createCustomerController', function() {
 
+    this.emails = [];
 
+    this.addEmail = function() {
+      let email = this.emailLine;
+      if(email === '' || typeof email === undefined || typeof email == 'undefined') return;
+      this.emailLine = '';
+      this.emails.push(email);
+    };
 
   });
 })();
